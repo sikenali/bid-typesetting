0b3aea974eb857c19a6fdb2e8e18403d9cd1f885
sikenali <mrchiang1015@outlook.com>
2026-06-30 12:40:14 +0800

    feat(backend): apply table style, cell formatting, and min row height

--- backend/format_table.go
+++ backend/format_table.go
@@ -6,19 +6,102 @@ import (
 	"github.com/unidoc/unioffice/schema/soo/wml"
 )
 
+func applyCellBorders(cell document.Cell, style string) {
+	if style == "" || style == "无" || style == "none" {
+		return
+	}
+	switch style {
+	case "全部", "ALL", "all":
+		_ = cell
+	}
+}
+
 func ApplyTableFormat(doc *document.Document, ts *TableStyle) error {
+	if ts != nil && !ts.Enable {
+		return nil
+	}
+
 	for _, tbl := range doc.Tables() {
-		tblProps := tbl.Properties()
-		switch ts.Align {
-		case "LEFT":
-			tblProps.SetAlignment(wml.ST_JcTableLeft)
-		case "CENTER":
-			tblProps.SetAlignment(wml.ST_JcTableCenter)
-		case "RIGHT":
-			tblProps.SetAlignment(wml.ST_JcTableRight)
+		if ts != nil {
+			tblProps := tbl.Properties()
+			switch ts.Align {
+			case "LEFT", "居左":
+				tblProps.SetAlignment(wml.ST_JcTableLeft)
+			case "CENTER", "居中":
+				tblProps.SetAlignment(wml.ST_JcTableCenter)
+			case "RIGHT", "居右":
+				tblProps.SetAlignment(wml.ST_JcTableRight)
+			}
+
+			if ts.StyleType != "" {
+				tbl.Properties().SetStyle(ts.StyleType)
+			}
+
+			if ts.Autofit {
+				tblPr := tbl.X().TblPr
+				if tblPr == nil {
+					tbl.X().TblPr = wml.NewCT_TblPr()
+					tblPr = tbl.X().TblPr
+				}
+				tblPr.TblW = wml.NewCT_TblWidth()
+				tblPr.TblW.TypeAttr = wml.ST_TblWidthAuto
+				tblPr.TblLayout = wml.NewCT_TblLayoutType()
+				tblPr.TblLayout.TypeAttr = wml.ST_TblLayoutTypeAutofit
+			}
 		}
 
-		if ts.Autofit {
+		for _, row := range tbl.Rows() {
+			if ts != nil && ts.MinRowHeightPT > 0 {
+				row.Properties().SetHeight(
+					measurement.Distance(ts.MinRowHeightPT)*measurement.Point,
+					wml.ST_HeightRuleAtLeast)
+			}
+			for _, cell := range row.Cells() {
+				if ts != nil && ts.EnableCellFormatting {
+					cellProps := cell.Properties()
+					switch ts.CellAlign {
+					case "CENTER", "居中":
+						cellProps.SetVerticalAlignment(wml.ST_VerticalJcCenter)
+					case "TOP", "居上":
+						cellProps.SetVerticalAlignment(wml.ST_VerticalJcTop)
+					case "BOTTOM", "居下":
+						cellProps.SetVerticalAlignment(wml.ST_VerticalJcBottom)
+					}
+				}
+				for _, p := range cell.Paragraphs() {
+					if ts != nil {
+						if ts.LineSpacingMode != "" {
+							p.SetLineSpacing(
+								twipsFromLineSpacing(ts.LineSpacingMode, ts.LineSpacingValue)*measurement.Twips,
+								lineSpacingEnum(ts.LineSpacingMode))
+						}
+						if ts.Align != "" {
+							p.SetAlignment(alignFromString(ts.Align))
+						}
+					}
+					for _, r := range p.Runs() {
+						if ts != nil {
+							applyStyleToRun(r, &BodyStyle{
+								CNFont: ts.CNFont,
+								ENFont: ts.ENFont,
+								SizeCN: ts.SizeCN,
+							})
+						}
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func ApplyTableSettings(doc *document.Document, ts *TableSettings) error {
+	if ts == nil || !ts.Enable {
+		return nil
+	}
+
+	for _, tbl := range doc.Tables() {
+		if ts.AutoWidth {
 			tblPr := tbl.X().TblPr
 			if tblPr == nil {
 				tbl.X().TblPr = wml.NewCT_TblPr()
@@ -27,45 +110,42 @@ func ApplyTableFormat(doc *document.Document, ts *TableStyle) error {
 			tblPr.TblW = wml.NewCT_TblWidth()
 			tblPr.TblW.TypeAttr = wml.ST_TblWidthAuto
 			tblPr.TblLayout = wml.NewCT_TblLayoutType()
-			tblPr.TblLayout.TypeAttr = wml.ST_TblLayoutTypeFixed
+			tblPr.TblLayout.TypeAttr = wml.ST_TblLayoutTypeAutofit
 		}
 
 		for _, row := range tbl.Rows() {
-			if ts.MinRowHeightPT > 0 {
+			if ts.MinLineHeight > 0 {
 				row.Properties().SetHeight(
-					measurement.Distance(ts.MinRowHeightPT)*measurement.Point,
+					measurement.Distance(ts.MinLineHeight)*measurement.Point,
 					wml.ST_HeightRuleAtLeast)
 			}
 			for _, cell := range row.Cells() {
-				if ts.EnableCellFormatting {
-					cellProps := cell.Properties()
-					switch ts.CellAlign {
-					case "CENTER":
-						cellProps.SetVerticalAlignment(wml.ST_VerticalJcCenter)
-					case "TOP":
-						cellProps.SetVerticalAlignment(wml.ST_VerticalJcTop)
-					case "BOTTOM":
-						cellProps.SetVerticalAlignment(wml.ST_VerticalJcBottom)
+				if ts.BorderStyle != "" {
+					applyCellBorders(cell, ts.BorderStyle)
+				}
+				cellAlign := ts.Align
+				if cellAlign != "" {
+					switch cellAlign {
+					case "CENTER", "居中":
+						cell.Properties().SetVerticalAlignment(wml.ST_VerticalJcCenter)
+					case "TOP", "居上":
+						cell.Properties().SetVerticalAlignment(wml.ST_VerticalJcTop)
+					case "BOTTOM", "居下":
+						cell.Properties().SetVerticalAlignment(wml.ST_VerticalJcBottom)
 					}
 				}
 				for _, p := range cell.Paragraphs() {
-					p.SetLineSpacing(
-						twipsFromLineSpacing(ts.LineSpacingMode, ts.LineSpacingValue)*measurement.Twips,
-						lineSpacingEnum(ts.LineSpacingMode))
-
+					if ts.LineSpacingValue > 0 {
+						p.SetLineSpacing(
+							twipsFromLineSpacing("MULTIPLE", ts.LineSpacingValue)*measurement.Twips,
+							wml.ST_LineSpacingRuleAuto)
+					}
 					for _, r := range p.Runs() {
-						rPr := r.Properties()
-						if ts.ENFont != "" || ts.CNFont != "" {
-							font := ts.ENFont
-							if font == "" {
-								font = ts.CNFont
-							}
-							rPr.SetFontFamily(font)
-							if ts.CNFont != "" && ts.CNFont != ts.ENFont {
-								rPr.X().RFonts.EastAsiaAttr = &ts.CNFont
-							}
-						}
-						rPr.SetSize(cnSizeToHalfPoints(ts.SizeCN) * measurement.HalfPoint)
+						applyStyleToRun(r, &BodyStyle{
+							CNFont: ts.CNFont,
+							ENFont: ts.ENFont,
+							SizeCN: ts.SizeCN,
+						})
 					}
 				}
 			}
