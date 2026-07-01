94ca0d56daea9535270eca953a468ff3d638c975
sikenali <mrchiang1015@aliyun.com>
2026-07-01 11:11:32 +0800

    fix(router): register /template route for Template page

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Upload from '../views/Upload.vue'
 import Editor from '../views/Editor.vue'
 import Compare from '../views/Compare.vue'
 import Settings from '../views/Settings.vue'
+import Template from '../views/Template.vue'
 
 const routes = [
   { path: '/', component: Upload },
   { path: '/editor', component: Editor },
   { path: '/compare', component: Compare },
   { path: '/settings', component: Settings },
+  { path: '/template', component: Template },
 ]
 
 const router = createRouter({
